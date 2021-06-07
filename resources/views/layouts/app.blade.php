ecabcd41f06fb920c3c3e3015504c9892eea1299
euclides ollarves <euclidesollarves@gmail.com>
2021-06-07 03:00:56 -0430

    favicon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,7 +8,7 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <title>{{ config('app.name', 'Laravel') }}</title>
-
+    <link rel="icon" type="image/png" href="{{url('favicon.ico')}}">
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
 
